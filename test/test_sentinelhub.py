e68b89b400afad9c089278fb2736a7e6938d8c99
Norman Fomferra <norman.fomferra@gmail.com>
2019-06-13 18:03:38 +0200

    Tests now work

--- test/test_sentinelhub.py
+++ test/test_sentinelhub.py
@@ -1,28 +1,55 @@
 import json
 import os
+import time
 import unittest
 from typing import Dict
 
 from xcube_dcfs.sentinelhub import SentinelHub
 
+HAS_SH_CREDENTIALS = 'SH_CLIENT_ID' in os.environ and 'SH_CLIENT_SECRET' in os.environ
+REQUIRE_SH_CREDENTIALS = 'requires SH credentials'
+
 
 class SentinelHubTest(unittest.TestCase):
 
-    @unittest.skipUnless('SH_CLIENT_ID' in os.environ and 'SH_CLIENT_SECRET' in os.environ,
-                         "requires SH credentials")
-    def test_process(self):
-        with open_res('evalscript.js') as fp:
-            evalscript = fp.read()
-
-        with open_res('request.json') as fp:
+    @unittest.skipUnless(HAS_SH_CREDENTIALS, REQUIRE_SH_CREDENTIALS)
+    def test_get_data_single(self):
+        with open_res('request-single.json') as fp:
             request = json.load(fp)
-            request['evalscript'] = evalscript
-
-        print(json.dumps(request, indent=2))
 
         sentinel_hub = SentinelHub()
-        response = sentinel_hub.process(request)
-        print(response)
+
+        t1 = time.perf_counter()
+        mime_type, data = sentinel_hub.get_data(request)
+        t2 = time.perf_counter()
+        print(f"test_get_data_single: took {t2 - t1} secs")
+
+        self.assertEqual('application/tar', mime_type)
+        # self.assertEqual('image/tif', mime_type)
+
+        with open('response-single.tif', 'wb') as fp:
+            fp.write(data)
+
+        sentinel_hub.close()
+
+    @unittest.skipUnless(HAS_SH_CREDENTIALS, REQUIRE_SH_CREDENTIALS)
+    def test_get_data_multi(self):
+        with open_res('request-multi.json') as fp:
+            request = json.load(fp)
+
+        sentinel_hub = SentinelHub()
+
+        t1 = time.perf_counter()
+        mime_type, data = sentinel_hub.get_data(request)
+        t2 = time.perf_counter()
+        print(f"test_get_data_multi: took {t2 - t1} secs")
+
+        self.assertEqual('application/tar', mime_type)
+
+        with open('response-multi.tar', 'wb') as fp:
+            fp.write(data)
+
+        sentinel_hub.close()
 
     def test_dataset_names(self):
         expected_dataset_names = ["DEM", "S2L1C", "S2L2A", "CUSTOM", "S1GRD"]
@@ -60,6 +87,7 @@ class SentinelHubTest(unittest.TestCase):
             }
         }))
         self.assertEqual(expected_band_names, sentinel_hub.band_names('S2L2A'))
+        sentinel_hub.close()
 
     def test_token_info(self):
         expected_token_info = {
@@ -81,17 +109,69 @@ class SentinelHubTest(unittest.TestCase):
             }
         }))
         self.assertEqual(expected_token_info, sentinel_hub.token_info)
+        sentinel_hub.close()
+
+    def test_new_data_request_single(self):
+        request = SentinelHub.new_data_request(
+            'S2L1C',
+            ['B02'],
+            (512, 512),
+            time_range=("2018-10-01T00:00:00.000Z", "2018-10-10T00:00:00.000Z"),
+            bbox=(
+                13.822,
+                45.850,
+                14.559,
+                46.291,
+            ),
+            sample_types="INT8",
+        )
+
+        # with open('request-single.json', 'w') as fp:
+        #     json.dump(request, fp, indent=2)
+
+        with open_res('request-single.json') as fp:
+            expected_request = json.load(fp)
+
+        self.assertEqual(expected_request, request)
+
+    def test_new_data_request_multi(self):
+        request = SentinelHub.new_data_request(
+            'S2L1C',
+            ['B02', 'B03', 'B04', 'B08'],
+            (512, 512),
+            time_range=("2018-10-01T00:00:00.000Z", "2018-10-10T00:00:00.000Z"),
+            bbox=(
+                13.822,
+                45.850,
+                14.559,
+                46.291,
+            ),
+            sample_types="INT8",
+        )
+
+        # with open('request-multi.json', 'w') as fp:
+        #     json.dump(request, fp, indent=2)
+
+        with open_res('request-multi.json') as fp:
+            expected_request = json.load(fp)
+
+        self.assertEqual(expected_request, request)
 
 
 class SessionMock:
     def __init__(self, mapping: Dict):
         self.mapping = mapping
 
-    def get(self, url):
+    # noinspection PyUnusedLocal
+    def get(self, url, **kwargs):
         return self._response(self.mapping['get'][url])
 
-    def post(self, url):
+    # noinspection PyUnusedLocal
+    def post(self, url, **kwargs):
         return self._response(self.mapping['post'][url])
+
+    def close(self):
+        pass
 
     @classmethod
     def _response(cls, obj):
--- xcube_dcfs/sentinelhub.py
+++ xcube_dcfs/sentinelhub.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import List, Any, Dict
+from typing import List, Any, Dict, Tuple, Union, Sequence
 
 import oauthlib.oauth2
 import requests_oauthlib
@@ -35,6 +35,9 @@ class SentinelHub:
             self.session = session
             self.token = None
 
+    def close(self):
+        self.session.close()
+
     @property
     def token_info(self) -> Dict[str, Any]:
         resp = self.session.get(self.oauth2_url + '/tokeninfo')
@@ -52,7 +55,144 @@ class SentinelHub:
         obj = json.loads(resp.content)
         return obj.get('data')
 
-    def process(self, request: Dict):
-        resp = self.session.post(self.api_url + f'/process', json=request)
-        obj = json.loads(resp.content)
-        return obj
+    def get_data(self, request: Dict) -> Tuple[str, Any]:
+        outputs = request['output']['responses']
+        accept_header = 'application/tar'  # if len(outputs) > 1 else 'image/tiff'
+        resp = self.session.post(self.api_url + f'/process', json=request,
+                                 headers={
+                                     'Accept': accept_header,
+                                     'cache-control': 'no-cache'
+                                 })
+
+        if not resp.ok:
+            raise SentinelHubError(resp.reason,
+                                   status_code=resp.status_code,
+                                   content=resp.content)
+
+        return accept_header, resp.content
+
+    @classmethod
+    def new_data_request(cls,
+                         dataset_name: str,
+                         band_names: Sequence[str],
+                         size: Tuple[int, int],
+                         time_range: Tuple[str, str] = None,
+                         bbox: Tuple[float, float, float, float] = None,
+                         upsampling: str = 'BILINEAR',
+                         downsampling: str = 'BILINEAR',
+                         band_units: Union[str, Sequence[str]] = 'reflectance',
+                         sample_types: Union[str, Sequence[str]] = 'FLOAT32') -> Dict:
+
+        if bbox is None:
+            bbox = [-180., -90., 180., 90.]
+
+        if isinstance(band_units, str):
+            band_units = [band_units] * len(band_names)
+
+        if isinstance(sample_types, str):
+            sample_types = [sample_types] * len(band_names)
+
+        data_element = {
+            "type": dataset_name,
+            "processing": {
+                "upsampling": upsampling,
+                "downsampling": downsampling
+            },
+        }
+
+        if time_range is not None:
+            time_range_from, time_range_to = time_range
+            time_range_element = {}
+            if time_range_from:
+                time_range_element['from'] = str(time_range_from)
+            if time_range_to:
+                time_range_element['to'] = str(time_range_to)
+            data_element["dataFilter"] = dict(timeRange=time_range_element)
+
+        input_element = {
+            "bounds": {
+                "bbox": bbox,
+                "properties": {
+                    "crs": "http://www.opengis.net/def/crs/OGC/9.5.3/CRS84"
+                }
+            },
+            "data": [data_element]
+        }
+
+        width, height = size
+        responses_element = []
+
+        for band_name in band_names:
+            responses_element.append({
+                "identifier": band_name,
+                "format": {
+                    "type": "image/tiff"
+                }
+            })
+
+        output_element = {
+            "width": width,
+            "height": height,
+            "responses": responses_element
+        }
+
+        evalscript = []
+        evalscript.extend([
+            "//VERSION=3",
+            "function setup() {",
+            "    return {",
+            "        input: [{",
+            "            bands: [" + ", ".join(map(repr, band_names)) + "],",
+            "            units: [" + ", ".join(map(repr, band_units)) + "],",
+            "        }],",
+            "        output: [",
+        ])
+        evalscript.extend(["            {id: " + repr(band_name) + ", bands: 1, sampleType: " + repr(sample_type) + "},"
+                           for band_name, sample_type in zip(band_names, sample_types)])
+        evalscript.extend([
+            "        ]",
+            "    };",
+            "}"
+        ])
+        # if len(band_names) > 1:
+        if len(band_names) > 0:
+            evalscript.extend([
+                "function evaluatePixel(sample) {",
+                "    return {",
+            ])
+            evalscript.extend(["        " + band_name + ": [sample." + band_name + "]," for band_name in band_names])
+            evalscript.extend([
+                "    };",
+                "}",
+            ])
+        else:
+            # Doesn't work, buts docs say so: https://docs.sentinel-hub.com/api/latest/#/Evalscript/V3/README
+            evalscript.extend([
+                "function evaluatePixel(sample) {",
+                "    return [sample." + band_names[0] + "];",
+                "}",
+            ])
+
+        # Convert into valid JSON
+        return json.loads(json.dumps({
+            "input": input_element,
+            "output": output_element,
+            "evalscript": "\n".join(evalscript)
+        }))
+
+
+class SentinelHubError(Exception):
+    def __init__(self, reason, status_code, content=None):
+        super().__init__(reason)
+        self.reason = reason
+        self.status_code = status_code
+        self.content = content
+
+    def __repr__(self) -> str:
+        return f'SentinelHubError({self.reason}, {self.status_code}, details={self.content!r})'
+
+    def __str__(self) -> str:
+        text = f'{self.reason}, status code {self.status_code}'
+        if self.content:
+            text += f':\n{self.content}\n'
+        return text
